5d94380833b72488464db25f12f9d117092ec39b
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-04-14 13:45:46 +0300

    Enhance component structure by updating About, Home, and NavBar with detailed JSX and styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    About
+    <h2>About Me</h2>
+    <p>Hi! I'm a passionate web developer currently learning full-stack development through an intensive bootcamp. 
+      I love building interactive, user-friendly applications and am especially excited about JavaScript and React. 
+      When I’m not coding, you’ll probably find me watching football or exploring new tech tools!</p>
+    <img src={image} alt="I made this"/>
+    </div>;
 }
 
 export default About;
